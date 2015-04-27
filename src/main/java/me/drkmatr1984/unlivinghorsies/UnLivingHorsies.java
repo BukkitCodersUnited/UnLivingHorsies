b7698584db4b2ad61cb5983f89b8d6bb18c0cd87
Nicolas Shreiner <DrkMatr1984@aol.com>
2015-04-27 02:11:59 -0400

    Add per-world Toggling & Compile Against 1.8.3

--- src/main/java/me/drkmatr1984/unlivinghorsies/UnLivingHorsies.java
+++ src/main/java/me/drkmatr1984/unlivinghorsies/UnLivingHorsies.java
@@ -1,10 +1,16 @@
 package me.drkmatr1984.unlivinghorsies;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
 
 public class UnLivingHorsies extends JavaPlugin
 {
@@ -12,14 +18,29 @@ public class UnLivingHorsies extends JavaPlugin
   public static int horseToSkeletonChance;
   public static int spawnChance;
   public static int horseToZombieChance;
+  public static List<String> enabledWorldString = new ArrayList<String>();
+  public static ArrayList<World> enabledWorlds = new ArrayList<World>();
   PluginManager pm = getServer().getPluginManager();
+  public static Plugin plugin;
 
   public void onEnable() {
     getConfig().options().copyDefaults(true);
     saveConfig();
     initConfig();
+    plugin = this;
     pm.registerEvents(new me.drkmatr1984.unlivinghorsies.hListener(), this);
-    getServer().getConsoleSender().sendMessage("Â§F[Â§5UnLivingHorsiesÂ§F] Loaded!Â§r");
+    if(enabledWorlds.isEmpty())
+    {
+    	getServer().getConsoleSender().sendMessage("§4ERROR: No existing worlds defined in config.yml. Disabling...");
+    	plugin.getServer().getPluginManager().disablePlugin(plugin);
+    }else{
+    	getServer().getConsoleSender().sendMessage("Â§F[Â§5UnLivingHorsies v0.2Â§F] §2Loaded!§FÂ§r");
+    }   
+  }
+  
+  public void onDisable()
+  {
+	getServer().getConsoleSender().sendMessage("Â§F[Â§5UnLivingHorsies v0.2Â§F] §4Disabled!§FÂ§r");
   }
 
   public void initConfig()
@@ -28,6 +49,17 @@ public class UnLivingHorsies extends JavaPlugin
     skeletonTozombieChance = getConfig().getInt("Natural Spawn skeleton to zombie horse chance");
     horseToZombieChance = getConfig().getInt("Horse death to zombie horse chance");
     horseToSkeletonChance = getConfig().getInt("Horse death to skeleton horse chance");
+    enabledWorldString = getConfig().getStringList("EnabledWorlds");
+    for(String world : enabledWorldString)
+    {
+        World enabled = Bukkit.getServer().getWorld(world);
+        if(enabled == null)
+        {
+        	getServer().getConsoleSender().sendMessage("§4ERROR: World§F " + world + " §4not found! §5UnLivingHorsies §4will not function!");
+        }else{
+        	enabledWorlds.add(enabled);
+        }       
+    }
   }
 
   public static void spawnSkeletonHorse(Location l) {
--- src/main/java/me/drkmatr1984/unlivinghorsies/hListener.java
+++ src/main/java/me/drkmatr1984/unlivinghorsies/hListener.java
@@ -2,6 +2,7 @@ package me.drkmatr1984.unlivinghorsies;
 
 import java.util.Random;
 
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 import org.bukkit.event.EventHandler;
@@ -15,35 +16,47 @@ public class hListener implements Listener{
 public static Random random = new Random();
 @EventHandler
   public void onCreatureSpawn(CreatureSpawnEvent e) {
-    if((e.getSpawnReason() == SpawnReason.CHUNK_GEN) || (e.getSpawnReason() == SpawnReason.NATURAL) || (e.getSpawnReason() == SpawnReason.BREEDING) || (e.getSpawnReason() == SpawnReason.CUSTOM) || (e.getSpawnReason() == SpawnReason.SPAWNER) || (e.getSpawnReason() == SpawnReason.SPAWNER_EGG) || (e.getSpawnReason() == SpawnReason.DEFAULT)){
-	  if (e.getEntityType() == EntityType.HORSE) {
-		  int a = random.nextInt(100) + 1;
-		  int b = random.nextInt(100) + 1;
-		  if (a <= UnLivingHorsies.spawnChance){
-			  if (b <= UnLivingHorsies.skeletonTozombieChance) {
-				  UnLivingHorsies.spawnSkeletonHorse(e.getEntity().getLocation());
-				  e.getEntity().remove();
-			  }else{
-			  UnLivingHorsies.spawnZombieHorse(e.getEntity().getLocation());
-			  e.getEntity().remove();
+	for(World world : UnLivingHorsies.enabledWorlds)
+	{
+		if(e.getEntity().getWorld() == world)
+		{
+			if((e.getSpawnReason() == SpawnReason.CHUNK_GEN) || (e.getSpawnReason() == SpawnReason.NATURAL) || (e.getSpawnReason() == SpawnReason.BREEDING) || (e.getSpawnReason() == SpawnReason.CUSTOM) || (e.getSpawnReason() == SpawnReason.SPAWNER) || (e.getSpawnReason() == SpawnReason.SPAWNER_EGG) || (e.getSpawnReason() == SpawnReason.DEFAULT)){
+		    	if (e.getEntityType() == EntityType.HORSE) {
+				  int a = random.nextInt(100) + 1;
+				  int b = random.nextInt(100) + 1;
+				  if (a <= UnLivingHorsies.spawnChance){
+					  if (b <= UnLivingHorsies.skeletonTozombieChance) {
+						  UnLivingHorsies.spawnSkeletonHorse(e.getEntity().getLocation());						  
+						  e.getEntity().remove();
+					  }else{
+					  UnLivingHorsies.spawnZombieHorse(e.getEntity().getLocation());
+					  e.getEntity().remove();
+					  }
+				  }  
 			  }
-		  }  
-	  }
-    }
+		    }
+		}
+	}
 	  return;
   }
 
   @EventHandler
   public void onEntityDeath(EntityDeathEvent e) {
-    if (e.getEntityType() == EntityType.HORSE) {
-      Horse h = (Horse)e.getEntity();
-      if ((h.getVariant() == Horse.Variant.SKELETON_HORSE) || (h.getVariant() == Horse.Variant.UNDEAD_HORSE)) return;
-      int c = random.nextInt(200) + 1;
-      if ((c <= UnLivingHorsies.horseToSkeletonChance) && (UnLivingHorsies.horseToSkeletonChance != 0)) {
-    	  UnLivingHorsies.spawnSkeletonHorse(e.getEntity().getLocation());
-      }
-      if ((c >= 100) && (c <= 100 + UnLivingHorsies.horseToZombieChance) && (UnLivingHorsies.horseToZombieChance != 0))
-    	  UnLivingHorsies.spawnZombieHorse(e.getEntity().getLocation());
-      }
+	  for(World world : UnLivingHorsies.enabledWorlds)
+		{
+			if(e.getEntity().getWorld() == world)
+			{
+				if (e.getEntityType() == EntityType.HORSE) {
+			      Horse h = (Horse)e.getEntity();
+			      if ((h.getVariant() == Horse.Variant.SKELETON_HORSE) || (h.getVariant() == Horse.Variant.UNDEAD_HORSE)) return;
+			      int c = random.nextInt(200) + 1;
+			      if ((c <= UnLivingHorsies.horseToSkeletonChance) && (UnLivingHorsies.horseToSkeletonChance != 0)) {
+			    	  UnLivingHorsies.spawnSkeletonHorse(e.getEntity().getLocation());
+			      }
+			      if ((c >= 100) && (c <= 100 + UnLivingHorsies.horseToZombieChance) && (UnLivingHorsies.horseToZombieChance != 0))
+			    	  UnLivingHorsies.spawnZombieHorse(e.getEntity().getLocation());
+			      }
+			  }
+		}
   }
 }
